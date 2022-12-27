be40d60fdf32edb2e6b4d5dd060be06d4efc3963
brignoli-pitt <pierre.brignoli@gmail.com>
2022-12-27 18:11:04 +0100

    add fixtures with Faker

--- src/DataFixtures/SeasonFixtures.php
+++ src/DataFixtures/SeasonFixtures.php
@@ -4,27 +4,37 @@ namespace App\DataFixtures;
 
 use App\Entity\Season;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+
+use Faker\Factory;
 
 class SeasonFixtures extends Fixture implements DependentFixtureInterface
 {
-    const NUMBERS = [1,2,3,4,5];
-    const YEARS = [];
-
     public function load(ObjectManager $manager): void
     {
-        $season = new Season();
-        $season->setNumber('1');
-        $season->setYear('2010');
-        $season->setDescription('première saison');
-        $season->setProgram($this->getReference('program_1'));
-        $manager->persist($season);
+        $faker = Factory::create();
+
+        /**
+         * L'objet $faker que tu récupères est l'outil qui va te permettre
+         * de te générer toutes les données que tu souhaites.
+         */
+
+        for($i = 0; $i < 50; $i++) {
+            $season = new Season();
+            $season->setNumber($faker->numberBetween(1, 10));
+            $season->setYear($faker->year());
+            $season->setDescription($faker->paragraphs(3, true));
+            $season->setProgram($this->getReference('program_' . $faker->numberBetween(0, 4)));
+            $this->addReference('season_' . $i, $season);
+
+            $manager->persist($season);
+        }
 
         $manager->flush();
     }
 
-    public function getDependencies()
+    public function getDependencies(): array
     {
         return [
             ProgramFixtures::class,
